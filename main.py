422c130d6e3133a5e820eeb71b24103bc4360ca3
Carlos André <casl2@aluno.ifal.edu.br>
2024-04-24 18:11:05 -0300

    dicionary add

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 # Criar instância FastAPI
 app = FastAPI()
+
+questions = {
+
+	"Matemática": {
+
+		"geometria": "1+1?",
+		"aritimética": "1+1?"
+	},
+
+	"Física": {
+
+		"geometria": "2+2?",
+		"aritimética": "2+2?"
+	},
+}
 
 @app.get("/") # <- usamos get para obter informações
 def questionAnswer():
@@ -10,3 +25,9 @@ def questionAnswer():
 	que pedimos
 	"""
 	return {"Quanto é 1+1?": "2"}
+
+
+@app.get("/get-question/{disciplina}") # <- usamos get para obter informações
+def get_question(disciplina: str = Path(None)):
+	return questions[disciplina]
+	
