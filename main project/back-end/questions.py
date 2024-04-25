bb8c573532eb2022ac6ee97a1adab62bc29b3342
CLL <96326019+CllsPy@users.noreply.github.com>
2024-04-25 06:46:42 +0000

    Update questions.py

--- main project/back-end/questions.py
+++ main project/back-end/questions.py
@@ -4,7 +4,7 @@ class Questoes:
 	
 		{"matéria": "matemática", "pergunta": "qual sen30º?", "resposta": "1/2"},
 		{"matéria": "biologia", "pergunta": "qual sentido da vida?", "resposta": "42"},
-		{"matéria": "física", "pergunta": "qual idade do raposo?", "resposta": "12"},
+		{"matéria": "física", "pergunta": "qual idade do lucas?", "resposta": "12"},
 		{"matéria": "conhecimentos gerais", "pergunta":"quem mora em AL?", "resposta": "ALA-GÔ-Á"}
 	]
 	
